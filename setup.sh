21652120413cf98ecfc56d62f589d812d676be01
udbhav1 <umuthakana@gmail.com>
2022-03-14 23:33:38 -0400

    add precompiled akura bpf program

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@
 # to preload dex bpf program and save deploy time
 # for some reason i cant interact w the dex program if akura is also deployed this way
 
+solana config set --url localhost
+
 echo "AIRDROPPING -----------------------------------------------------------------------------"
 solana airdrop 100 2RALhrsY7uHg55QWGWE9RPXjmMAAtbkGsekE1PZYdDJP
 
 echo "DEPLOYING -------------------------------------------------------------------------------"
-anchor test --skip-local-validator
+anchor test --skip-build --skip-local-validator
